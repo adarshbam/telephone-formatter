a32eefe163b4c9cbd9fd9b464a5a3bb8e28775c3
Adarsh Singh <adarshbambahadur@gmail.com>
2025-09-22 13:47:00 +0530

    formatting and remove formatting working properly

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const phoneNumber = document.querySelector("#phone-number");
+let formatted = false;
+
+phoneNumber.addEventListener("keypress", (e) => {
+  if (!/[0-9]/.test(e.key)) {
+    e.preventDefault(); // block non-numeric keys
+  }
+});
 
 phoneNumber.addEventListener("input", () => {
-  console.log(phoneNumber.value);
+  let number = phoneNumber.value;
+
+  number.includes("+") ? (formatted = true) : (formatted = false);
+  console.log(number, formatted);
+  if (number.length >= 3 && formatted == false) {
+    number =
+      "+(" + number.slice(0, 3) + ") - " + number.slice(3, number.length);
+  } else if (formatted == true && number.length <= 9) {
+    number = number.slice(2, 5);
+  }
+  console.log(number);
+  phoneNumber.value = number;
 });
